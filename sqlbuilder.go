0859ed7f5b9a97ef6d3365b5021d5e1d228a9b30
司锐 <srandroid@163.com>
2020-11-27 21:00:53 +0800

    修改

--- sqlbuilder.go
+++ sqlbuilder.go
@@ -1,30 +1,123 @@
 package sqlbuilder
 
-
 import (
 	"bytes"
 	"strings"
 )
 
-type sqlBuilder struct {
+type builder struct {
 	sql    bytes.Buffer
 	values []interface{}
 	where  bool
 	link   bool
 }
 
-func NewSqlBuilder() *sqlBuilder {
-	t := new(sqlBuilder)
+type selectBuilder struct {
+	builder
+}
+
+type insertBuilder struct {
+	builder
+}
+
+type updateBuilder struct {
+	builder
+}
+
+type deleteBuilder struct {
+	builder
+}
+
+func NewSelect() *selectBuilder {
+	t := new(selectBuilder)
 	return t
 }
 
-func (m *sqlBuilder) Select(cols ...string) *sqlBuilder {
+func NewInsert() *insertBuilder {
+	t := new(insertBuilder)
+	return t
+}
+
+func NewUpdate() *updateBuilder {
+	t := new(updateBuilder)
+	return t
+}
+
+func NewDelete() *deleteBuilder {
+	t := new(deleteBuilder)
+	return t
+}
+
+//===================================插入语句==============================================
+//插入SQL生成器
+func (m *insertBuilder) Insert(table string) *insertBuilder {
+	m.sql.WriteString("INSERT INTO ")
+	m.sql.WriteString(table)
+	m.sql.WriteString(" ")
+	return m
+}
+
+//插入SQL生成器
+func (m *insertBuilder) Columns(cols ...string) *insertBuilder {
+	m.sql.WriteString("(")
+	m.sql.WriteString(strings.Join(cols, ", "))
+	m.sql.WriteString(") ")
+	return m
+}
+
+//插入SQL生成器
+func (m *insertBuilder) Values(cols ...interface{}) *insertBuilder {
+	m.sql.WriteString("VALUES (")
+	for i, _ := range cols {
+		m.sql.WriteString("?")
+		if len(cols)-1 != i {
+			m.sql.WriteString(", ")
+		}
+	}
+	m.sql.WriteString(") ")
+	m.values = append(m.values, cols...)
+	return m
+}
+
+//======================================更新语句===============================================
+//更新SQL生成器
+func (m *updateBuilder) Update(table string) *updateBuilder {
+	m.sql.WriteString("UPDATE ")
+	m.sql.WriteString(table)
+	m.sql.WriteString(" ")
+	return m
+}
+
+//更新SQL生成器
+func (m *updateBuilder) Set(cols ...string) *updateBuilder {
+	m.sql.WriteString("SET ")
+	for i, c := range cols {
+		m.sql.WriteString(c)
+		m.sql.WriteString(" = ?")
+		if len(cols)-1 != i {
+			m.sql.WriteString(", ")
+		}
+	}
+	return m
+}
+
+//======================================删除语句==============================================
+//更新SQL生成器
+func (m *deleteBuilder) Delete() *deleteBuilder {
+	m.sql.WriteString("DELETE ")
+	return m
+}
+
+//=========================================查询语句============================================
+//查询SQL生成器
+func (m *selectBuilder) Select(cols ...string) *selectBuilder {
 	m.sql.WriteString("SELECT ")
 	m.sql.WriteString(strings.Join(cols, ", "))
 	return m
 }
 
-func (m *sqlBuilder) From(table string) *sqlBuilder {
+//查询SQL生成器
+func (m *selectBuilder) From(table string) *selectBuilder {
 	m.sql.WriteString(" FORM ")
 	m.sql.WriteString(table)
 	m.sql.WriteString(" a ")
@@ -32,7 +125,9 @@ func (m *sqlBuilder) From(table string) *sqlBuilder {
 	return m
 }
 
-func (m *sqlBuilder) Where(query string, value interface{}) *sqlBuilder {
+//=======================================公共条件================================================
+//SQL通用条件生成器
+func (m *builder) Where(query string, value interface{}) *builder {
 	if value == nil || value == "" {
 		return m
 	}
@@ -47,7 +142,38 @@ func (m *sqlBuilder) Where(query string, value interface{}) *sqlBuilder {
 	return m
 }
 
-func (m *sqlBuilder) Join(join string, value ...interface{}) *sqlBuilder {
+//SQL IN条件生成器
+func (m *builder) In(query string, value []interface{}) *builder {
+	if len(value) == 0 {
+		return m
+	}
+	m.And()
+	for i, _ := range value {
+		m.sql.WriteString("?")
+		if len(value)-1 != i {
+			m.sql.WriteString(", ")
+		}
+	}
+	m.sql.WriteString(query)
+	m.values = append(m.values, value...)
+	m.link = true
+	return m
+}
+
+//SQL LIKE条件生成器
+func (m *builder) Like(query string, value interface{}) *builder {
+	if value == nil || value == "" {
+		return m
+	}
+	m.And()
+	m.sql.WriteString(strings.Replace(query, "?", "CONCAT('%',?,'%')", -1))
+	m.values = append(m.values, value)
+	m.link = true
+	return m
+}
+
+//SQL表连接生成器
+func (m *builder) Join(join string, value ...interface{}) *builder {
 	m.sql.WriteString(join)
 	if len(value) > 0 {
 		m.values = append(m.values, value...)
@@ -55,16 +181,19 @@ func (m *sqlBuilder) Join(join string, value ...interface{}) *sqlBuilder {
 	return m
 }
 
-func (m *sqlBuilder) Append(query string) *sqlBuilder {
+//追加任意SQL
+func (m *builder) Append(query string) *builder {
 	m.sql.WriteString(query)
 	return m
 }
 
-func (m *sqlBuilder) Build() (string, []interface{}) {
+//返回最终SQL及参数
+func (m *builder) Build() (string, []interface{}) {
 	return m.sql.String(), m.values
 }
 
-func (m *sqlBuilder) And() *sqlBuilder {
+//手动增加AND
+func (m *builder) And() *builder {
 	if m.link {
 		m.sql.WriteString("AND ")
 		m.link = false
@@ -72,7 +201,8 @@ func (m *sqlBuilder) And() *sqlBuilder {
 	return m
 }
 
-func (m *sqlBuilder) Or() *sqlBuilder {
+//手动增加OR
+func (m *builder) Or() *builder {
 	m.sql.WriteString("AND ")
 	return m
 }
